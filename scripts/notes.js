30816f20a8b7b5588b81becbb4c779873943bc21
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-01-21 00:34:08 -0300

    transpose working

--- scripts/notes.js
+++ scripts/notes.js
@@ -1,12 +1,26 @@
-export let majorNote = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-export let minorNote = ['Cm', 'C#m', 'Dm', 'D#m', 'Em', 'Fm', 'F#m', 'Gm', 'G#m', 'Am', 'A#m', 'Bm']
+export let majorNote = []
+export let minorNote = []
+
+const majorMemory = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+const minorMemory = ['Cm', 'C#m', 'Dm', 'D#m', 'Em', 'Fm', 'F#m', 'Gm', 'G#m', 'Am', 'A#m', 'Bm']
 
 transpose()
 function transpose() {
     console.log('transpose')
-    let transp = Math.floor(Math.random() * 12)
-    // console.log(transp)
+    let transp = Math.ceil(Math.random() * 12)
+
+    console.log(transp)
+    
     for(let i = 0; i < 12; i++){
-        majorNote[i] = 'C'
+        if((i + transp) > 12){
+            majorNote[i] = majorMemory[i + transp - 12 - 1]
+            minorNote[i] = minorMemory[i + transp - 12 - 1]
+            
+        }
+        else{
+            majorNote[i] = majorMemory[i + transp - 1]
+            minorNote[i] = minorMemory[i + transp - 1]
+            
+        }
     }
 }
