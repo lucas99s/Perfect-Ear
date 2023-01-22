a94bbd164d2725038204a6e3ccc49fc8f509f523
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-01-22 15:42:28 -0300

    style updates, embedded video clip and added show/hide correct notes

--- musics/saturn-sleeping-at-last.js
+++ musics/saturn-sleeping-at-last.js
@@ -16,7 +16,7 @@ console.log(majorNote)
 export let saturnTone = musicNotes[2]
 export let saturn =
     `
-    <div>
+    <div class="container-stanza">
         <pre>  ${musicNotes[3]}             ${musicNotes[0]}         ${musicNotes[2]}              ${musicNotes[1]}</pre>
         <div class="input-container"><input type="text" value="                                                                                                            "></div>
         <div class="stanza">You taught me the courage of stars before you left</div>
@@ -28,6 +28,9 @@ export let saturn =
         <pre>  ${musicNotes[3]}             ${musicNotes[0]}         ${musicNotes[2]}              ${musicNotes[1]}</pre>
         <div class="input-container"><input type="text" value="                                                                                                            "></div>
         <div class="stanza">With shortness of breath, you explained the infinite</div>
+    </div>
+    <div class="container-video">
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/dzNvk80XY9s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
     `
 
