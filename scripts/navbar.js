f454ac2cda6a171f707a56d690cbc1d6af7f9b8c
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-01-21 22:43:17 -0300

    navbar added

---
+++ scripts/navbar.js
@@ -0,0 +1,9 @@
+import { getId } from "./library.js";
+
+getId('navbar').innerHTML = `
+<ul>
+    <li><a href="index.html">Início</a></li>
+    <li><a href="music.html">Música</a></div>
+    <li><a href="#">Sobre</a></li>
+</ul>
+`
