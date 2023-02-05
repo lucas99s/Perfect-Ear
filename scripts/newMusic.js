7d7ef6c02f3707c24e9a10c12cd5c3862aa2544e
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-02-04 22:46:58 -0300

    difference between lyrics and chords

--- scripts/newMusic.js
+++ scripts/newMusic.js
@@ -2,19 +2,21 @@ import { majorMemory } from "./notes.js";
 
 export function processMusic() {
     var rows = document.querySelector('textarea').value.split("\n");
-
     for(let i = 0; i < rows.length; i++) {
-        if(true){
+        if(!rows[i].includes('/')){
             for(let j = 0; j < majorMemory.length; j++){
                 if(rows[i].includes(majorMemory[j])) {
                     let row = rows[i]
-                    
-                    rows[i] = row.replace(majorMemory[j], 'majorMemory['+j+']')
-                    console.log(rows)
+                    rows[i] = row.replace(majorMemory[j], 'majorChords['+j+']')
                 }
             }
+            rows[i] = `<div>${rows[i]}</div>`
+        } else if(rows[i].includes('/')) {
+            let row = rows[i]
+            rows[i] = row.replaceAll('/', '')
+            rows[i] = `<div>${rows[i]}</div>`
         }
     }
-
-    // console.log(rows)
+    console.log(rows)
+    document.getElementById('display').innerHTML = rows
 }
