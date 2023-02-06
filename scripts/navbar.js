02ba0b896ef3edb261a70f3443838ba22f16d9f6
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-02-05 23:56:07 -0300

    home banner created and navbar updated

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,8 +3,8 @@ import { getId } from "./library.js";
 getId('navbar').innerHTML = `
 <ul>
     <li><a href="index.html">Início</a></li>
-    <li><a href="music.html">Música</a></div>
-    <li><a href="newMusic.html">Nova música</a></div>
+    <li><a href="music.html">Me desafie</a></div>
+    <li><a href="newMusic.html">Crie</a></div>
     <li><a href="#">Sobre</a></li>
 </ul>
 `
