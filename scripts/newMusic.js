2e3dac0c57260553a242e93dac73e138375cb674
Lucas Rodrigues <lucasdeveloper99s@gmail.com>
2023-02-06 23:56:03 -0300

    splitting each note into a new array and identifying sharps and flats

--- scripts/newMusic.js
+++ scripts/newMusic.js
@@ -3,20 +3,43 @@ import { majorMemory } from "./notes.js";
 export function processMusic() {
     var rows = document.querySelector('textarea').value.split("\n");
     for(let i = 0; i < rows.length; i++) {
-        if(!rows[i].includes('/')){
-            for(let j = 0; j < majorMemory.length; j++){
-                if(rows[i].includes(majorMemory[j])) {
-                    let row = rows[i]
-                    rows[i] = row.replace(majorMemory[j], 'majorChords['+j+']')
+        if(!rows[i].includes('*')){
+            let chord = rows[i].split(' ')
+            let subs = ''
+            chord.forEach(obj => {
+                if(obj != ''){
+                    for(let j = 0; j < majorMemory.length; j++){
+                        if(obj.includes(majorMemory[j])) {
+                            if(obj.includes('#')) {
+                                subs += obj.replace(majorMemory[j], `majorChords[${j + 1}]`)
+                            } else if(obj.includes('b')) {
+                                subs += obj.replace(majorMemory[j], `majorChords[${ j - 1}]`)
+                            } else {
+                                subs += obj.replace(majorMemory[j], 'majorChords['+j+']')
+                            }
+                            console.log(subs)
+                        }
+                    }
+                } else if(obj == '') {
+                    console.log(subs)
+                    subs += ' '
                 }
-            }
+            })
+            subs = subs.replaceAll('#', '')
+            subs = subs.replaceAll('b', '')
+            rows[i] = subs
             rows[i] = `<div>${rows[i]}</div>`
-        } else if(rows[i].includes('/')) {
+        } else if(rows[i].includes('*')) {
             let row = rows[i]
-            rows[i] = row.replaceAll('/', '')
+            rows[i] = row.replaceAll('*', '')
             rows[i] = `<div>${rows[i]}</div>`
         }
     }
     console.log(rows)
     document.getElementById('display').innerHTML = rows
+}
+
+
+function getPosition(string, subString, index) {
+    return string.split(subString, index).join(subString).length;
 }
